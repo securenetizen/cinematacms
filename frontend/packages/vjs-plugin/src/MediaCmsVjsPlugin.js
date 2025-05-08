400ccc792312e39b73b688a6c0cf9e3128cb6add
Adryan Eka Vandra <adryanekavandra@gmail.com>
2025-05-08 12:59:45 +0700

    fix: enhance plugin registration and add children prop to MediaListRow component
    - Updated the `MediaCmsVjsPlugin` to check for the existence of `videojs.registerPlugin` and fallback to `videojs.plugin` for compatibility.
    - Added a `children` prop to the `MediaListRow` component to allow for nested elements.

--- frontend/packages/vjs-plugin/src/MediaCmsVjsPlugin.js
+++ frontend/packages/vjs-plugin/src/MediaCmsVjsPlugin.js
@@ -3620,7 +3620,12 @@ function generatePlugin(/*videojs*/) {
 
 	MediaCmsVjsPlugin.VERSION = VERSION;
 
-	videojs.registerPlugin("mediaCmsVjsPlugin", MediaCmsVjsPlugin);
+
+	if (typeof videojs.registerPlugin === 'function') {
+		videojs.registerPlugin('mediaCmsVjsPlugin', MediaCmsVjsPlugin);
+	} else {
+		videojs.plugin('mediaCmsVjsPlugin', MediaCmsVjsPlugin);
+	}
 
 
 	return MediaCmsVjsPlugin;
