91332031135569707d85e9d9e9e8ec900dcf6d5e
Jay <jccruz009@yahoo.com>
2025-06-03 18:41:49 +0800

    fix: exclude /admin/login endpoint in cms.urls

--- cms/urls.py
+++ cms/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import path, re_path
 from django.conf.urls import include
 from django.conf import settings
 import debug_toolbar
 
+def redirect_admin_login(request):
+  return HttpResponseRedirect('/')
+
 urlpatterns = [
     re_path(r"^__debug__/", include(debug_toolbar.urls)),
+    path('admin/login/', redirect_admin_login), # Excluded for security purposes
     path(settings.DJANGO_ADMIN_URL, admin.site.urls),
     re_path(r"^", include("files.urls")),
     re_path(r"^", include("users.urls")),
