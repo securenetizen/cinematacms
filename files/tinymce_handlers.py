b67adb4f97a437d0a556e9fa5cbc534be8aa40ce
John Henry Galino <github@jhgalino.com>
2025-06-17 06:31:57 +0000

    Remove media upload
    Only image upload is supported for drag and drop in TinyMCE, so this code is not needed.

--- files/tinymce_handlers.py
+++ files/tinymce_handlers.py
@@ -1,10 +1,5 @@
-import os
-from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.core.files.storage import default_storage
-from django.core.files.base import ContentFile
-from django.utils import timezone
 from .models import TinyMCEMedia
 
 @csrf_exempt
@@ -25,22 +20,3 @@ def upload_image(request):
                 'location': media.url
             })
     return JsonResponse({'error': 'Invalid request'}, status=400)
-
-@csrf_exempt
-def upload_media(request):
-    if request.method == "POST":
-        file_obj = request.FILES.get('file')
-        if file_obj:
-            # Create a new TinyMCEMedia instance for the media file
-            media = TinyMCEMedia(
-                file=file_obj,
-                file_type='media',
-                original_filename=file_obj.name,
-                user=request.user if request.user.is_authenticated else None
-            )
-            media.save()
-            
-            return JsonResponse({
-                'location': media.url
-            })
-    return JsonResponse({'error': 'Invalid request'}, status=400) 
