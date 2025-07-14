d5babaef10eccf0cbd4dbc677e4a89b260839a2c
Ashraf Haque <securenetizen@proton.me>
2025-07-14 23:11:00 +0600

    Update settings.py

--- cms/settings.py
+++ cms/settings.py
@@ -140,7 +140,7 @@ LOGGING = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mediacms_prod",
+        "NAME": "mediacms",
         "HOST": "127.0.0.1",
         "PORT": "5432",
         "USER": "mediacms",
