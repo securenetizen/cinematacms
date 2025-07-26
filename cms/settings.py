efe9800a2efee6cfc974cff6aa7213da9d9e5d13
Jay <jccruz009@yahoo.com>
2025-07-26 15:16:52 +0800

    fix: move additional mfa configs to line 482

--- cms/settings.py
+++ cms/settings.py
@@ -537,6 +537,11 @@ MFA_REQUIRED_ROLES = ['superuser', 'manager']
 MFA_ENFORCE_ON_PATHS = [f'/{DJANGO_ADMIN_URL}']
 MFA_EXCLUDE_PATHS = ['/fu/', '/api/', '/manage/', '/accounts/']
 
+# additional MFA-permission configs
+MFA_REQUIRED_ROLES = ['superuser']
+MFA_ENFORCE_ON_PATHS = [f'/{DJANGO_ADMIN_URL}']
+MFA_EXCLUDE_PATHS = ['/fu/', '/api/', '/manage/', '/accounts/']
+
 WHISPER_CPP_DIR, WHISPER_CPP_COMMAND, WHISPER_CPP_MODEL = get_whisper_cpp_paths()
 from .local_settings import *
 ALLOWED_HOSTS.append(FRONTEND_HOST.replace("http://", "").replace("https://", ""))
